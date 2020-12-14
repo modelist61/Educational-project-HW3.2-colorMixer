052eebdc97bfe15ada92697ad156678cd618ee5c
Tokarev Dmitry <modelist61@bk.ru>
2020-12-14 11:02:16 +0300

    Initial Commit

---
+++ HW3.2 colorMixer/HW3_2_colorMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_2_colorMixerApp.swift
+//  HW3.2 colorMixer
+//
+//  Created by Dmitry Tokarev on 14.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct HW3_2_colorMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
